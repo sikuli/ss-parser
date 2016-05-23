0896352efff105e4c901f487331bd61e16b2d04b
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-05-23 14:08:35 -0600

    add unit converters

---
+++ test/unit-converter.spec.js
@@ -0,0 +1,16 @@
+const unitConverter = require('../lib/utils/unit-converter');
+const expect = require('chai').expect;
+
+describe('unit converter', () => {
+  it('converts 1465 emu to pixels', () => {
+    expect(unitConverter.emuToPixels(20000)).to.equal(2);
+  });
+
+  it('converts 0 emu to pixels', () => {
+    expect(unitConverter.emuToPixels(0)).to.equal(0);
+  });
+
+  it('converts 1200 whole points to points', () => {
+    expect(unitConverter.wholePointsToPoints(1200)).to.equal(12);
+  });
+});
