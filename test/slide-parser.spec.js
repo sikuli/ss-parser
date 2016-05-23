47bc322bef894dec564c92c466419f8398eebfb6
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-05-23 13:09:53 -0600

    parse part of slide files

---
+++ test/slide-parser.spec.js
@@ -0,0 +1,52 @@
+const expect = require('chai').expect;
+const Promise = require('bluebird');
+const fs = require('fs');
+const mkdtemp = Promise.promisify(fs.mkdtemp);
+const path = require('path');
+const os = require('os');
+const rmdir = require('rimraf');
+const decompress = require('decompress');
+const slideParser = require('../lib/parsers/slide-parser');
+
+describe('slide parser', () => {
+  let targetDir = path.join(os.tmpDir(), 'sikuli.slides-');
+  const pptxFile = path.join(__dirname, 'fixtures', 'simple.pptx');
+
+  before((done) => {
+    mkdtemp(targetDir)
+      .then((dir) => {
+        targetDir = dir;
+        return decompress(pptxFile, targetDir);
+      })
+      .then(() => done())
+      .catch((e) => done(e));
+  });
+
+  after((done) => {
+    rmdir(targetDir, (err) => {
+      done(err);
+    });
+  });
+
+  it('parses a slide with a target and an action', (done) => {
+    const slideFile = path.join(targetDir, 'ppt', 'slides', 'slide2.xml');
+    const expected = {
+      slide: 2,
+      screenshot: path.join(targetDir, 'ppt', 'media', 'image00.png'),
+      target: {
+        x: 20,
+        y: 40,
+        width: 200,
+        height: 400
+      },
+      action: 'Click',
+      displayItems: []
+    };
+    slideParser(slideFile)
+      .then((actual) => {
+        expect(actual).to.deep.equal(expected);
+        done();
+      })
+      .catch((e) => done(e));
+  });
+});
