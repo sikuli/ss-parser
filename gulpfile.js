7b902294438f403a9f9bf46df364cb34bd319d36
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-05-24 14:23:19 -0600

    ignore unused files

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
 gulp.task('lint', () => {
-  gulp.src(['*.js', 'lib/**/*.js', 'test/**/*.js'])
+  gulp.src(['lib/**/*.js', 'test/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
