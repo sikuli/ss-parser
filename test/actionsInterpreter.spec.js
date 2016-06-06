f4fbb7c9ee9977a93e8229e279ab6936c4af3a82
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-06-06 17:08:20 -0600

    get all actions

--- test/actionsInterpreter.spec.js
+++ test/actionsInterpreter.spec.js
@@ -5,80 +5,110 @@ const actionsInterpreter = require('../lib/interpreters/actionsInterpreter');
 describe('Actions interpreter:', () => {
   it('interprets mouse actions using names and aliases', () => {
     // Left click
-    expect(actionsInterpreter('click')).to.deep.equal({ name: 'click', value: undefined });
-    expect(actionsInterpreter('left-click')).to.deep.equal({ name: 'click', value: undefined });
-    expect(actionsInterpreter('leftClick')).to.deep.equal({ name: 'click', value: undefined });
-    expect(actionsInterpreter('left click')).to.deep.equal({ name: 'click', value: undefined });
+    expect(actionsInterpreter.lookup('click')).to.deep.equal(
+      { name: 'click', value: undefined });
+    expect(actionsInterpreter.lookup('left-click')).to.deep.equal(
+      { name: 'click', value: undefined });
+    expect(actionsInterpreter.lookup('leftClick')).to.deep.equal(
+      { name: 'click', value: undefined });
+    expect(actionsInterpreter.lookup('left click')).to.deep.equal(
+      { name: 'click', value: undefined });
     // Right click
-    expect(actionsInterpreter('right-click')).to.deep.equal(
+    expect(actionsInterpreter.lookup('right-click')).to.deep.equal(
       { name: 'rightclick', value: undefined });
-    expect(actionsInterpreter('rightClick')).to.deep.equal(
+    expect(actionsInterpreter.lookup('rightClick')).to.deep.equal(
       { name: 'rightclick', value: undefined });
-    expect(actionsInterpreter('right click')).to.deep.equal(
+    expect(actionsInterpreter.lookup('right click')).to.deep.equal(
       { name: 'rightclick', value: undefined });
     // Double click
-    expect(actionsInterpreter('double-click')).to.deep.equal(
+    expect(actionsInterpreter.lookup('double-click')).to.deep.equal(
       { name: 'doubleclick', value: undefined });
-    expect(actionsInterpreter('doubleClick')).to.deep.equal(
+    expect(actionsInterpreter.lookup('doubleClick')).to.deep.equal(
       { name: 'doubleclick', value: undefined });
-    expect(actionsInterpreter('double click')).to.deep.equal(
+    expect(actionsInterpreter.lookup('double click')).to.deep.equal(
       { name: 'doubleclick', value: undefined });
     // Drag
-    expect(actionsInterpreter('drag')).to.deep.equal({ name: 'drag', value: undefined });
+    expect(actionsInterpreter.lookup('drag')).to.deep.equal(
+      { name: 'drag', value: undefined });
     // Drop
-    expect(actionsInterpreter('drop')).to.deep.equal({ name: 'drop', value: undefined });
+    expect(actionsInterpreter.lookup('drop')).to.deep.equal(
+      { name: 'drop', value: undefined });
     // Mouseover
-    expect(actionsInterpreter('mouseover')).to.deep.equal({ name: 'hover', value: undefined });
-    expect(actionsInterpreter('hover')).to.deep.equal({ name: 'hover', value: undefined });
-    expect(actionsInterpreter('mouse hover')).to.deep.equal({ name: 'hover', value: undefined });
-    expect(actionsInterpreter('mouse-hover')).to.deep.equal({ name: 'hover', value: undefined });
+    expect(actionsInterpreter.lookup('mouseover')).to.deep.equal(
+      { name: 'hover', value: undefined });
+    expect(actionsInterpreter.lookup('hover')).to.deep.equal(
+      { name: 'hover', value: undefined });
+    expect(actionsInterpreter.lookup('mouse hover')).to.deep.equal(
+      { name: 'hover', value: undefined });
+    expect(actionsInterpreter.lookup('mouse-hover')).to.deep.equal(
+      { name: 'hover', value: undefined });
   });
 
   it('interprets keyboard actions using names and aliases', () => {
     // Type
-    expect(actionsInterpreter('type')).to.deep.equal({ name: 'type', value: undefined });
+    expect(actionsInterpreter.lookup('type')).to.deep.equal(
+      { name: 'type', value: undefined });
     // Keypress
-    expect(actionsInterpreter('keypress')).to.deep.equal({ name: 'keypress', value: undefined });
-    expect(actionsInterpreter('press')).to.deep.equal({ name: 'keypress', value: undefined });
-    expect(actionsInterpreter('key-press')).to.deep.equal({ name: 'keypress', value: undefined });
+    expect(actionsInterpreter.lookup('keypress')).to.deep.equal(
+      { name: 'keypress', value: undefined });
+    expect(actionsInterpreter.lookup('press')).to.deep.equal(
+      { name: 'keypress', value: undefined });
+    expect(actionsInterpreter.lookup('key-press')).to.deep.equal(
+      { name: 'keypress', value: undefined });
   });
 
   it('interprets desktop actions using names and aliases', () => {
     // Open
-    expect(actionsInterpreter('open http://example.com')).to.deep.equal(
+    expect(actionsInterpreter.lookup('open http://example.com')).to.deep.equal(
       { name: 'open', value: 'http://example.com' });
     // Browse
-    expect(actionsInterpreter('browse http://example.com')).to.deep.equal(
+    expect(actionsInterpreter.lookup('browse http://example.com')).to.deep.equal(
       { name: 'browse', value: 'http://example.com' });
-    expect(actionsInterpreter('navigate http://example.com')).to.deep.equal(
+    expect(actionsInterpreter.lookup('navigate http://example.com')).to.deep.equal(
       { name: 'browse', value: 'http://example.com' });
-    expect(actionsInterpreter('visit http://example.com')).to.deep.equal(
+    expect(actionsInterpreter.lookup('visit http://example.com')).to.deep.equal(
       { name: 'browse', value: 'http://example.com' });
   });
 
   it('interprets custom actions using names and aliases', () => {
     // Exist
-    expect(actionsInterpreter('exist')).to.deep.equal({ name: 'exist', value: undefined });
-    expect(actionsInterpreter('exists')).to.deep.equal({ name: 'exist', value: undefined });
+    expect(actionsInterpreter.lookup('exist')).to.deep.equal(
+      { name: 'exist', value: undefined });
+    expect(actionsInterpreter.lookup('exists')).to.deep.equal(
+      { name: 'exist', value: undefined });
     // Not exist
-    expect(actionsInterpreter('not exist')).to.deep.equal({ name: 'notexist', value: undefined });
-    expect(actionsInterpreter('notExist')).to.deep.equal({ name: 'notexist', value: undefined });
+    expect(actionsInterpreter.lookup('not exist')).to.deep.equal(
+      { name: 'notexist', value: undefined });
+    expect(actionsInterpreter.lookup('notExist')).to.deep.equal(
+      { name: 'notexist', value: undefined });
     // Delay
-    expect(actionsInterpreter('delay 20 seconds')).to.deep.equal(
+    expect(actionsInterpreter.lookup('delay 20 seconds')).to.deep.equal(
       { name: 'delay', value: '20 seconds' }
     );
     // Wait
-    expect(actionsInterpreter('wait')).to.deep.equal({ name: 'wait', value: undefined });
+    expect(actionsInterpreter.lookup('wait')).to.deep.equal(
+      { name: 'wait', value: undefined });
     // Skip
-    expect(actionsInterpreter('skip')).to.deep.equal({ name: 'skip', value: undefined });
-    expect(actionsInterpreter('ignore')).to.deep.equal({ name: 'skip', value: undefined });
+    expect(actionsInterpreter.lookup('skip')).to.deep.equal(
+      { name: 'skip', value: undefined });
+    expect(actionsInterpreter.lookup('ignore')).to.deep.equal(
+      { name: 'skip', value: undefined });
     // Pause
-    expect(actionsInterpreter('pause')).to.deep.equal({ name: 'pause', value: undefined });
+    expect(actionsInterpreter.lookup('pause')).to.deep.equal(
+      { name: 'pause', value: undefined });
     // Stop
-    expect(actionsInterpreter('stop')).to.deep.equal({ name: 'stop', value: undefined });
+    expect(actionsInterpreter.lookup('stop')).to.deep.equal(
+      { name: 'stop', value: undefined });
   });
 
   it('should not interpret unknown actions', () => {
-    expect(actionsInterpreter('unknown')).to.deep.equal({ name: undefined, value: undefined });
+    expect(actionsInterpreter.lookup('unknown')).to.deep.equal(
+      { name: undefined, value: undefined });
+  });
+
+  it('should return all action names', () => {
+    const result = actionsInterpreter.getSupportedActions();
+    expect(result).to.have.length(37);
+    expect(result).to.include.members(['click', 'open']);
   });
 });
